aa770883f28c919566e2c06e7e05e5200f073e4d
Shaurya <singh.shauryadeep@gmail.com>
2020-05-12 15:02:43 +0530

    Login flow authentication complete

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 console.log('Hello');
+
+function login(username, password) {
+	if (username === 'shaur97' && password === 'password') {
+		alert('Login successful');
+	} else {
+		alert('Authentication failed!');
+	}
+}
